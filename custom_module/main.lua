feeb747f4dc1c356655db02e89b7e8d36bcbc90e
abayomi ogunnusi <abayomiogunnusi@gmail.com>
2023-07-10 04:28:43 +0100

    Chore: add custo module

---
+++ custom_module/main.lua
@@ -0,0 +1,4 @@
+local mod = require("mymath")
+
+print(mod.add(3,4))
+print(mod.substract(3,4))
---
+++ custom_module/mymath.lua
@@ -0,0 +1,11 @@
+mmath = {}
+
+function mmath.add(a,b)
+    return a + b
+end
+
+function mmath.substract(x,y)
+    return x - y
+end
+
+return mmath
